88bff39b54d05b73c5731d50353b94ed673c7f1f
RoiSukenik <roisukenik@gmail.com>
2021-01-15 22:45:57 +0200

    MessageAPI ADDED

---
+++ Ex4/Shared/MessageAPI.h
@@ -0,0 +1,11 @@
+#ifndef MESSAGEAPI_DOT_H
+#define MESSAGEAPI_DOT_H
+
+#include	<string.h>
+#include	<stdbool.h>
+#include	<stdlib.h>
+
+#define STATUS_CODE_FAILURE -1
+#define STATUS_CODE_SUCSESS  0
+
+#endif // !MESSAGEAPI_DOT_H
